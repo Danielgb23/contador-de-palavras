583e88c466a5132a47f864bd4c57ddcea893953f
Daniel Goncalves Benvenutti - 169448 <ra169448@le20-7.grad.fee.unicamp.br>
2019-02-28 09:37:17 -0300

    Adicionados pontos flutuantes e concertados problema com caracteres nao alfabeticos

--- src/main.c
+++ src/main.c
@@ -9,25 +9,36 @@
 
 int main() {
 
-  char c;
-  int words=1;
+	char c;
+	int words=0;
 
-  c = 1;
+	c = 1;
 
-  while (c != '\n') {
-    scanf("%c", &c);
-    if( c == ' ' || c == ',' || c == '.' || c == '!' || c == ';'){
-	while ( c == ' ' || c == ',' || c == '.' || c == '!' || c == ';')
+	while (c != '\n') {
 		scanf("%c", &c);
-	if (c == '\n')
-		break;
-	words++;
-}
 
-	
+		if ( c > 64 && c < 91 || c > 96 && c < 123 ){
+			while ( c > 64 && c < 91 || c > 96 && c < 123 )
+				scanf("%c", &c);
+			words++;
+		}
+		else if ( c > 47 && c < 58 ){
+			while ( c > 47 && c < 58 )
+				scanf("%c", &c);
+			if( c == '.' || c == ','  )	{
+				scanf("%c", &c);
+				while ( c > 47 && c < 58 )
+					scanf("%c", &c);
+			}
+			words++;
 
-  }
+		}	
+	}
 
-  printf("%d\n", words);
-  return 0;
+
+
+
+
+	printf("%d\n", words);
+	return 0;
 }
